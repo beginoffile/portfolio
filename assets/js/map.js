5cb8836638b436f40d21452691eedaa897ce1cd9
beginoffile <cursos.mapatupisaro@gmail.com>
2024-01-27 16:06:31 -0600

    finish contact page

---
+++ assets/js/map.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    setTimeout(() => {
+        document.querySelector("#load-iframe-map").innerHTML = `<iframe class="contact__iframe" frmaborder="0" scroling="no" marginheight="0" marginwidth="0" style="border:0;" loading="lazy" referrerpolicy="no-referrer-when-downgrade" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9475.826086648147!2d-96.8913905258166!3d19.5208182543436!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85db3233e254dbbb%3A0x13f293633cd717fe!2sCostco%20Wholesale%20Xalapa!5e0!3m2!1ses!2smx!4v1705971993165!5m2!1ses!2smx"></iframe>`;    
+    }, 500);
+    
+    
+});
